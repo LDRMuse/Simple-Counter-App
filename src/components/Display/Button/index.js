281feb5ec7a73d6a2ca8d3721e6668a0e1ddce4c
Melissa <melissakheying@gmail.com>
2020-07-29 13:29:17 -0500

    add notes to Button and Display to better understand what is happening in the code

--- src/components/Display/Button/index.js
+++ src/components/Display/Button/index.js
@@ -5,13 +5,13 @@ import "./Button.css"
 export const Button = (props) => {
   const handleClick = (event) => {
     props.buttonHandler(event)
-
+    //event means target
   }
 return <button onClick = {handleClick} data-increment={props.increment}>{props.buttonTxt}</button>
-
+// data- ties to dataset.increment ^
 
 }
-
+// These are the prop types above
 Button.propTypes = {
   buttonHandler: PropTypes.func,
   buttonTxt: PropTypes.string,
@@ -20,8 +20,6 @@ Button.propTypes = {
 
 Button.defaultProps = {
   buttonTxt: "Add 1",
-  increment: 1
-}
-
+  increment: 1 }
 
 
--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -10,6 +10,7 @@ export class Display extends React.Component {
     currentCount: 0,
   }
 
+  // the child button will send up the 'event' info via {target}
   updateCount = ({target}) => {
     this.setState(prevState => {
       return { currentCount: prevState.currentCount + Number(target.dataset.increment) }
@@ -22,7 +23,7 @@ export class Display extends React.Component {
       <div>
         <p>{this.state.currentCount}</p>
         <Button buttonHandler={this.updateCount} increment={1} buttonTxt="Add 1" />
-        <Button buttonHandler={this.updateCount} increment={2} buttonTxt="Add 1" />
+        <Button buttonHandler={this.updateCount} increment={2} buttonTxt="Add 2" />
       </div>
 
     )
